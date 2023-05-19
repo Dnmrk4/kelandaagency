2a8a5a2245291435df5562371235fbd2c0a8181f
Dnmrk4 <danmark.chemuren@gmail.com>
2023-05-19 19:42:56 +0300

    maps

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ function Footer() {
           >
             <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 256c-35.3 0-64-28.7-64-64s28.7-64 64-64s64 28.7 64 64s-28.7 64-64 64z" />
           </svg>
-          <span>Daima Tower, Northern Tower, 9th floor, Eldoret, Kenya</span>
+          <a href="https://goo.gl/maps/H3ZAP46cnJKV5Ph56"><span>Daima Tower, Northern Tower, 9th floor, Eldoret, Kenya</span></a>
         </p>
         <p className="text-xs ">© 2023 - kelanda international agency</p>
       </div>
